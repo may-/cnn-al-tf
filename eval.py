2f4c3f69cf6ff545090395a118255393b8800fcb
AD\mayumi.ohta <mayumi.ohta@zo.uni-heidelberg.de>
2016-09-26 20:13:46 +0200

    update

--- eval.py
+++ eval.py
@@ -2,7 +2,7 @@
 
 ##########################################################
 #
-# Convolutional Neural Network for Multi-label text classification
+# Evaluate Convolutional Neural Network
 #
 #
 #   Note: this implementation is mostly based on
@@ -25,6 +25,7 @@ this_dir = os.path.abspath(os.path.dirname(__file__))
 
 # eval parameters
 tf.app.flags.DEFINE_string('train_dir', os.path.join(this_dir, 'train'), 'Directory of the checkpoint files')
+tf.app.flags.DEFINE_float('threshold', 0.5, 'Threshold value. Must be one of np.linspace(0, 1, 11)')
 
 
 def evaluate(eval_data, config):
@@ -50,9 +51,7 @@ def evaluate(eval_data, config):
             print "Evaluating ..."
             if config.has_key('split') and config['split']:
                 left_batch, middle_batch, right_batch, y_batch, n_batch = zip(*eval_data)
-                feed = {m.left: np.array(left_batch),
-                        m.right: np.array(right_batch),
-                        m.labels: np.array(y_batch)}
+                feed = {m.left: np.array(left_batch), m.right: np.array(right_batch), m.labels: np.array(y_batch)}
             else:
                 x_batch, y_batch, n_batch = zip(*eval_data)
                 feed = {m.inputs: np.array(x_batch), m.labels: np.array(y_batch)}
@@ -61,11 +60,8 @@ def evaluate(eval_data, config):
             loss, eval = sess.run([m.total_loss, m.eval_op], feed_dict=feed)
             pre, rec = zip(*eval)
 
-            auc = util.calc_auc_pr(pre, rec)
-            f1 = (2.0 * pre[5] * rec[5]) / (pre[5] + pre[5])
-            print '%s: loss = %.6f, f1 = %.4f, auc = %.4f' % (datetime.now(), loss, f1, auc)
 
-    return pre, rec
+    return loss, pre, rec
 
 
 
@@ -75,12 +71,19 @@ def main(argv=None):
 
     if restore_param.has_key('split') and restore_param['split']:
         data = util.read_data_contextwise(restore_param['data_dir'], 'dev', restore_param['sent_len'],
-                                          negative=restore_param['negative'])
+                                          negative=restore_param['negative'], hierarchical=restore_param['hierarchical'])
     else:
         data = util.read_data(restore_param['data_dir'], 'dev', restore_param['sent_len'],
-                              negative=restore_param['negative'])
+                              negative=restore_param['negative'], hierarchical=restore_param['hierarchical'])
 
-    pre, rec = evaluate(data, restore_param)
+    loss, pre, rec = evaluate(data, restore_param)
+    auc = util.calc_auc_pr(pre, rec)
+    try:
+        idx = list(np.linspace(0, 1, 11)).index(FLAGS.threshold)
+    except ValueError:
+        idx = 5
+    f1 = (2.0 * pre[idx] * rec[idx]) / (pre[idx] + pre[idx])
+    print '%s: loss = %.6f, f1 = %.4f, auc = %.4f' % (datetime.now(), loss, f1, auc)
     util.dump_to_file(os.path.join(FLAGS.train_dir, 'results.cPickle'), {'precision': pre, 'recall': rec})
 
 
